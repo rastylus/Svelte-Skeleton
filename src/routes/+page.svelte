d693ab76f2166852f6a9132a55d8bd1336d6dac0
rastylus <ryan.p.riggs@gmail.com>
2024-01-30 08:14:12 -0600

    added routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,3 +2,6 @@
 </script>
 <h1>Svelte Skele</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Hello and welcome to my site!</h1>
+<a href="/about">About my site</a>
+
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,5 @@
+<!-- src/routes/about/+page.svelte -->
+
+<h1>About this site</h1>
+<p>TODO...</p>
+<a href="/">Home</a>
